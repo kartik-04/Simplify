4aad6e33e85b659ab6bf7d241db3544228bbee80
kartik-04 <04kartik04@gmail.com>
2025-04-19 19:31:57 +0530

    Fix: use relative path for CSS

--- Simplify/dist/Simplify.77bcb86c.js
+++
@@ -1,2 +0,0 @@
-let collapsibles=document.querySelectorAll(".collapsible");collapsibles.forEach(l=>l.addEventListener("click",function(){this.classList.toggle("collapsible--expanded")}));
-//# sourceMappingURL=Simplify.77bcb86c.js.map
