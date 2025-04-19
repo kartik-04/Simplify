f0e0c0fb12d4dfa430c881be2177b7d3a8618688
kartik-04 <04kartik04@gmail.com>
2025-04-19 17:34:14 +0530

    removed dist from .gitignore

---
+++ Simplify/dist/Simplify.77bcb86c.js
@@ -0,0 +1,2 @@
+let collapsibles=document.querySelectorAll(".collapsible");collapsibles.forEach(l=>l.addEventListener("click",function(){this.classList.toggle("collapsible--expanded")}));
+//# sourceMappingURL=Simplify.77bcb86c.js.map
